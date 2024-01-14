98527721788153082898c1e70b5fb3bc072ce915
hun23 <hunn2023@gmail.com>
2024-01-14 20:03:38 +0900

    week3: day5

--- ALGO/3CUMULATIVE-SUM/14719_빗물.py
+++ ALGO/3CUMULATIVE-SUM/14719_빗물.py
@@ -8,5 +8,17 @@ def println(s):
 H, W = map(int, input().split())
 heights = list(map(int, input().split()))
 
-for h in heights:
-    
+prefix_max = [0] * W
+prefix_max[0] = heights[0]
+suffix_max = [0] * W
+suffix_max[-1] = heights[-1]
+for i in range(1, W):
+    h = heights[i]
+    prefix_max[i] = h if h > prefix_max[i - 1] else prefix_max[i - 1]
+for i in range(W - 2, -1, -1):
+    h = heights[i]
+    suffix_max[i] = h if h > suffix_max[i + 1] else suffix_max[i + 1]
+ans = 0
+for i in range(W):
+    ans += (min(prefix_max[i], suffix_max[i]) - heights[i])
+println(ans)
